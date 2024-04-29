1f7208d93fbbee37e91e6ab993260ac324d60379
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-04-29 12:22:25 +0200

    Update paths

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const newRouter = require('./routes/new');
 
 const app = express();
 
@@ -20,7 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/new', newRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
