ab3d57fb859f742382951c23aee6a02820157a8a
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-08-15 22:42:45 +0200

    add message controller

--- app.js
+++ app.js
@@ -1,31 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const { Pool } = require('pg');
-require('dotenv').config();
-const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
-const indexRouter = require('./routes/index');
-
-const pool = new Pool({
-  host: PGHOST,
-  database: PGDATABASE,
-  username: PGUSER,
-  password: PGPASSWORD,
-  port: 5432,
-  ssl: {
-    require: true,
-  },
-});
-async function getPgVersion() {
-  const client = await pool.connect();
-  try {
-    const result = await client.query('SELECT version()');
-    console.log(result.rows[0]);
-  } finally {
-    client.release();
-  }
-}
-getPgVersion();
+const indexRouter = require('./routes/indexRouter');
 
 const app = express();
 
@@ -34,7 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
---
+++ controllers/messageController.js
@@ -0,0 +1,28 @@
+const asyncHandler = require('express-async-handler');
+const db = require('../db/queries');
+
+const messageListGet = asyncHandler(async (req, res) => {
+  const messages = await db.getMessages();
+  res.render('index', {
+    title: 'Home',
+    messages: messages,
+  });
+});
+
+const messageCreateGet = asyncHandler(async (req, res) => {
+  res.render('form', {
+    title: 'Post message',
+  });
+});
+
+const messageCreatePost = asyncHandler(async (req, res) => {
+  const { username, comment } = req.body;
+  await db.insertMessages(username, comment);
+  res.redirect('/');
+});
+
+module.exports = {
+  messageListGet,
+  messageCreateGet,
+  messageCreatePost,
+};
