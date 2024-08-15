00c53048ce2ae523f11094a41d89fe5651872ca9
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-08-15 19:57:31 +0200

    Add database connection

--- app.js
+++ app.js
@@ -3,8 +3,31 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const { Pool } = require('pg');
+require('dotenv').config();
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 const indexRouter = require('./routes/index');
+
+const pool = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
+});
+async function getPgVersion() {
+  const client = await pool.connect();
+  try {
+    const result = await client.query('SELECT version()');
+    console.log(result.rows[0]);
+  } finally {
+    client.release();
+  }
+}
+getPgVersion();
 
 const app = express();
 
